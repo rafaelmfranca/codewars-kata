0d652219d0c6b526759430e7903f3092f86b3de2
Rafael França <rafaelmonfardinifranca@gmail.com>
2021-11-02 18:56:45 -0300

    :sparkles: - New kata solutions

---
+++ 8-kyu/Double Char/doubleChar.js
@@ -0,0 +1,8 @@
+const doubleChar = (str) => str.split('').map(ch => ch += ch).join('');
+
+console.log(doubleChar("abcd"), "aabbccdd");
+console.log(doubleChar("Adidas"), "AAddiiddaass");
+console.log(doubleChar("1337"), "11333377");
+console.log(doubleChar("illuminati"), "iilllluummiinnaattii");
+console.log(doubleChar("123456"), "112233445566");
+console.log(doubleChar("%^&*("), "%%^^&&**((");
