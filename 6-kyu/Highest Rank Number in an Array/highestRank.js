221d26cd2773ff7c0af87fe81fb8b1a5d0581ea1
Rafael França <rafaelmonfardinifranca@gmail.com>
2021-11-09 19:40:15 -0300

    :sparkles: - New kata

---
+++ 6-kyu/Highest Rank Number in an Array/highestRank.js
@@ -0,0 +1,9 @@
+function highestRank(numbers) {
+    let obj = {};
+    numbers.forEach((number) => obj[number] ? obj[number]++ : obj[number] = 1);
+    return Object.keys(obj).reduce((a, b) => obj[a] > obj[b] ? a : b);
+}
+
+console.log(highestRank([12, 10, 8, 12, 7, 6, 4, 10, 12]));
+console.log(highestRank([12, 10, 8, 12, 7, 6, 4, 10, 12, 10]));
+console.log(highestRank([12, 10, 8, 8, 3, 3, 3, 3, 2, 4, 10, 12, 10]));
