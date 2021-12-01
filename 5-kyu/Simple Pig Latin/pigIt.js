efa10a9684309c23f88b5afd68269ef5e41187e5
Rafael França <rafaelmonfardinifranca@gmail.com>
2021-11-30 21:01:53 -0300

    :sparkles: Simple Pig Latin solution (works with english ponctuation marks)

---
+++ 5-kyu/Simple Pig Latin/pigIt.js
@@ -0,0 +1,4 @@
+const pigIt = (str) =>  str.split(' ').map((word) => !/[^\w\s]/.test(word) ? `${word.substr(1)}${word.charAt(0)}ay` : word).join(' ');  
+
+console.log(pigIt('Pig latin is ! cool'),'igPay atinlay siay ! oolcay')
+console.log(pigIt('This is my string'),'hisTay siay ymay tringsay')
