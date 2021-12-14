e7ad3f5cf715ff329152e9c19a08ec3c7eb58d40
Rafael França <rafaelmonfardinifranca@gmail.com>
2021-12-13 23:35:10 -0300

    :sparkles: Added Coding Meetup #4 kata solution

---
+++ 7-kyu/Coding Meetup #4/getFirstPython.js
@@ -0,0 +1,18 @@
+const getFirstPython = (list) => {
+  const found = list.find(({ language }) => language === 'Python');
+  return found ? `${found.firstName}, ${found.country}` : 'There will be no Python developers';
+};
+
+var list1 = [
+  { firstName: 'Mark', lastName: 'G.', country: 'Scotland', continent: 'Europe', age: 22, language: 'JavaScript' },
+  { firstName: 'Victoria', lastName: 'T.', country: 'Puerto Rico', continent: 'Americas', age: 30, language: 'Python' },
+  { firstName: 'Emma', lastName: 'B.', country: 'Norway', continent: 'Europe', age: 19, language: 'Clojure' }
+];
+
+var list2 = [
+  { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 29, language: 'JavaScript' },
+  { firstName: 'Amar', lastName: 'V.', country: 'Bosnia and Herzegovina', continent: 'Europe', age: 32, language: 'Ruby' },
+];
+
+console.log(getFirstPython(list1), 'Victoria, Puerto Rico');
+console.log(getFirstPython(list2), 'There will be no Python developers');
