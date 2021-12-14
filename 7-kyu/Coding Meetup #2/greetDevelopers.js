3eb781af7728d06cbfb68220b2448741fc77b89b
Rafael França <rafaelmonfardinifranca@gmail.com>
2021-12-13 23:04:31 -0300

    :sparkles: Added Coding Meetup #2 kata solution

---
+++ 7-kyu/Coding Meetup #2/greetDevelopers.js
@@ -0,0 +1,25 @@
+const greetDevelopers = (list) => {
+  list.forEach((dev) => dev.greeting = `Hi ${dev.firstName}, what do you like the most about ${dev.language}?`)
+  return list;
+};
+
+const list1 = [
+  { firstName: 'Sofia', lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java' },
+  { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: 'Python' },
+  { firstName: 'Madison', lastName: 'U.', country: 'United States', continent: 'Americas', age: 32, language: 'Ruby' } 
+];
+
+const answer = [
+  { firstName: 'Sofia', lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java',
+    greeting: 'Hi Sofia, what do you like the most about Java?'
+  },
+  { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: 'Python',
+    greeting: 'Hi Lukas, what do you like the most about Python?'
+  },
+  { firstName: 'Madison', lastName: 'U.', country: 'United States', continent: 'Americas', age: 32, language: 'Ruby',
+    greeting: 'Hi Madison, what do you like the most about Ruby?'
+  } 
+];
+
+
+console.log(greetDevelopers(list1), answer);
