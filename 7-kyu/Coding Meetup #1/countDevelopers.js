833c5c49b0869d57daa41d2061b7bd382aeee9f8
Rafael França <rafaelmonfardinifranca@gmail.com>
2021-12-13 22:28:48 -0300

    :sparkles: Added Coding Metup #1 kata solution

---
+++ 7-kyu/Coding Meetup #1/countDevelopers.js
@@ -0,0 +1,16 @@
+const countDevelopers = (list) => list.filter(({ continent, language }) => continent === 'Europe' && language === 'JavaScript').length;
+
+const list1 = [
+  { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'JavaScript' },
+  { firstName: 'Maia', lastName: 'S.', country: 'Tahiti', continent: 'Oceania', age: 28, language: 'JavaScript' },
+  { firstName: 'Shufen', lastName: 'L.', country: 'Taiwan', continent: 'Asia', age: 35, language: 'HTML' },
+  { firstName: 'Sumayah', lastName: 'M.', country: 'Tajikistan', continent: 'Asia', age: 30, language: 'CSS' }
+];
+
+const list2 = [
+  { firstName: 'Oliver', lastName: 'Q.', country: 'Australia', continent: 'Oceania', age: 19, language: 'HTML' },
+  { firstName: 'Lukas', lastName: 'R.', country: 'Austria', continent: 'Europe', age: 89, language: 'HTML' }
+];
+
+console.log(countDevelopers(list1), 1);
+console.log(countDevelopers(list2), 0);
