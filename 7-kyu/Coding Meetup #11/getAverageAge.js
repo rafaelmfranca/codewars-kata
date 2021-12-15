1aca545fcf7aa165ac62827d7748fabf5a60d05f
Rafael França <rafaelmonfardinifranca@gmail.com>
2021-12-15 20:38:13 -0300

    :sparkles: Added Coding Meetup #11 kata solution

---
+++ 7-kyu/Coding Meetup #11/getAverageAge.js
@@ -0,0 +1,16 @@
+const getAverageAge = (list) => Math.round(list.reduce((acc, {age}) => (acc += age, acc), 0) / list.length);
+
+const list1 = [
+  { firstName: 'Maria', lastName: 'Y.', country: 'Cyprus', continent: 'Europe', age: 30, language: 'Java' },
+  { firstName: 'Victoria', lastName: 'T.', country: 'Puerto Rico', continent: 'Americas', age: 70, language: 'Python' },
+];
+
+console.log(getAverageAge(list1), 50);
+
+
+const list2 = [
+  { firstName: 'Noa', lastName: 'A.', country: 'Israel', continent: 'Asia', age: 20, language: 'Ruby' },
+  { firstName: 'Andrei', lastName: 'E.', country: 'Romania', continent: 'Europe', age: 21, language: 'C' },
+];
+
+console.log(getAverageAge(list2), 21);
