6fd372c7e84ec55579e64522fa6341b840547218
Rafael França <rafaelmonfardinifranca@gmail.com>
2021-12-15 20:40:12 -0300

    :recycle: Refactoring Coding Meetup #11 kata

--- 7-kyu/Coding Meetup #11/getAverageAge.js
+++ 7-kyu/Coding Meetup #11/getAverageAge.js
@@ -1,4 +1,4 @@
-const getAverageAge = (list) => Math.round(list.reduce((acc, {age}) => (acc += age, acc), 0) / list.length);
+const getAverageAge = (list) => Math.round(list.reduce((acc, {age}) => acc += age, 0) / list.length);
 
 const list1 = [
   { firstName: 'Maria', lastName: 'Y.', country: 'Cyprus', continent: 'Europe', age: 30, language: 'Java' },
