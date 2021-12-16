4945ab9d45806f40c4b92638102c04f436fdb229
Rafael França <rafaelmonfardinifranca@gmail.com>
2021-12-16 14:09:54 -0300

    :sparkles: Added Coding Meetup #16 kata solution

---
+++ 6-kyu/Coding Meetup #16/askForMissingDetails.js
@@ -0,0 +1,27 @@
+const askForMissingDetails = (list) => (
+  list.filter((dev) => Object.keys(dev).some((key) => !dev[key] && (dev.question = `Hi, could you please provide your ${key}.`)))
+)
+
+const list1 = [
+  { firstName: null, lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java' },
+  { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: null },
+  { firstName: 'Madison', lastName: 'U.', country: 'United States', continent: 'Americas', age: 32, language: 'Ruby' } 
+];
+
+const answer1 = [
+  { firstName: null, lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java', 
+  question: 'Hi, could you please provide your firstName.' },
+  { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: null, 
+  question: 'Hi, could you please provide your language.' }
+];
+
+const list2 = [
+  { firstName: 'Sofia', lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java' },
+  { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: 'Python' },
+  { firstName: 'Madison', lastName: 'U.', country: 'United States', continent: 'Americas', age: 32, language: 'Ruby' } 
+];
+
+const answer2 = [];
+
+console.log(askForMissingDetails(list1), answer1);
+console.log(askForMissingDetails(list2), answer2);
