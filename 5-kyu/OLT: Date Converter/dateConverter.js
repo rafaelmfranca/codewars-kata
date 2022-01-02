df37608fbd5581a2709bbcc6811eb47d2d0f403c
Rafael França <rafaelmonfardinifranca@gmail.com>
2022-01-02 11:21:03 -0300

    :sparkles: Added 'One Line Task: Date converter' kata solution

---
+++ 5-kyu/OLT: Date Converter/dateConverter.js
@@ -0,0 +1,18 @@
+const f = (inp) => `${month[inp.slice(-2)]}${inp.slice(2, 4)}`;
+
+const month = {
+  '01': 'Jan',
+  '02': 'Feb',
+  '03': 'Mar',
+  '04': 'Apr',
+  '05': 'May',
+  '06': 'Jun',
+  '07': 'Jul',
+  '08': 'Aug',
+  '09': 'Sep',
+  '10': 'Oct',
+  '11': 'Nov',
+  '12': 'Dec',
+};
+
+console.log(f('201809'));
