362ebf1fbe2e42622e6b044ac8953f3f34b71bfd
Rafael França <rafaelmonfardinifranca@gmail.com>
2022-01-20 19:41:53 -0300

    :sparkles: Added Descending Order kata solution

---
+++ 7-kyu/Descending Order/descendingOrder.js
@@ -0,0 +1,13 @@
+const descendingOrder = (n) =>
+  Number(
+    Array.from(String(n))
+      .sort((a, b) => b - a)
+      .join('')
+  );
+
+console.log(descendingOrder(0), 0);
+console.log(descendingOrder(1), 1);
+console.log(descendingOrder(111), 111);
+console.log(descendingOrder(15), 51);
+console.log(descendingOrder(1021), 2110);
+console.log(descendingOrder(123456789), 987654321);
